41fe7b3cf912a797a4bfa2988e6841a112c4a655
Shanoos <shanoos2@gmail.com>
2021-06-15 02:05:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ var app = new Vue({
 });
 
 //app.message = 'I have changed the data!';
+
+const app2 = new Vue({
+  el: '#app2',
+  data: {
+    costOfApples: 6,
+    costOfBananas: 2,
+    costOfCoconuts: 8
+  },
+  computed: {
+    totalAmount() {
+      return this.costOfApples + this.costOfBananas + this.costOfCoconuts
+    }
+  }
+})
